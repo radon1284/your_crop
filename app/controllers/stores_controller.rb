5cc661ffe3bf2b7c31db44aab5e8518fc6170ac1
Prelang Builder <builder@prelang.com>
2016-08-21 02:51:38 +0000

    Added #vote action to set model before_action in the votable model's controller

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -3,7 +3,7 @@ class StoresController < ApplicationController
   #->Prelang (scaffolding:rails/scope_to_user)
   before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
 
-  before_action :set_store, only: [:show, :edit, :update, :destroy]
+  before_action :set_store, only: [:show, :edit, :update, :destroy, :vote]
 
   # GET /stores
   # GET /stores.json
