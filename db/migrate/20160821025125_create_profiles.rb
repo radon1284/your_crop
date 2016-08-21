004e383129ce3b85851170fddb13561489c749de
Prelang Builder <builder@prelang.com>
2016-08-21 02:51:25 +0000

    Generated model Profile with migration

---
+++ db/migrate/20160821025125_create_profiles.rb
@@ -0,0 +1,13 @@
+class CreateProfiles < ActiveRecord::Migration
+  def change
+    create_table :profiles do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :middle_name
+      t.string :mobile_phone
+      t.string :telephone
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/profiles.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :profile do
+    first_name "MyString"
+    last_name "MyString"
+    middle_name "MyString"
+    mobile_phone "MyString"
+    telephone "MyString"
+  end
+end
